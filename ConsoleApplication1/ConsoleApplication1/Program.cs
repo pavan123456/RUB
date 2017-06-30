d0c72325a9973aeb37fb6b0bf384d7a0874bbcb3
Pavan Kasturi <pavan.kasturi@aveva.com>
2017-06-30 16:56:38 +0530

    [Examples2]

---
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    using System;
+    interface ICounter
+    {
+        void Increment();
+    }
+
+
+    struct Counter : ICounter
+    {
+        int value;
+        public override string ToString()
+        {
+            return value.ToString();
+        }
+        void ICounter.Increment()
+        {
+            value++;
+        }
+    }
+
+
+
+    class Program
+    {
+        static void Test<T>() where T : ICounter, new()
+        {
+            T x = new T();
+            Console.WriteLine(x);
+            x.Increment();
+            // Modify x
+
+
+            Console.WriteLine(x);
+
+            ((ICounter)x).Increment();
+
+            // Modify boxed copy of x
+
+            Console.WriteLine(x);
+            Console.ReadLine();
+        }
+        static void Main()
+        {
+            Test<Counter>();
+        }
+    }
+}
---
+++ ConsoleApplication1/GridTest/Account/ResetPasswordConfirmation.aspx.cs
@@ -0,0 +1,8 @@
+﻿using System.Web.UI;
+
+namespace GridTest.Account
+{
+    public partial class ResetPasswordConfirmation : Page
+    {
+    }
+}
