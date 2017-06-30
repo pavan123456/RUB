78d8c0d7aaabeb1ecf68a219b74a2bd4453b8cc6
Pavan Kasturi <pavan.kasturi@aveva.com>
2017-06-30 16:56:10 +0530

    [Examples]

---
+++ ArrayList/ArrayList/Default.aspx.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Collections;
+public partial class _Default : System.Web.UI.Page 
+{
+    private ArrayList obj = new ArrayList();
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        obj.Add("Shiv");
+        obj.Add("Raju");
+        obj.Add("Shiv1");
+        obj.Add("Shiv1");
+    }
+    protected void Button1_Click(object sender, EventArgs e)
+    {
+        foreach (string x in obj)
+        {
+            Response.Write(x);
+        }
+        DropDownList1.DataSource = obj;
+        DropDownList1.DataBind();
+        ListBox1.DataSource = obj;
+        ListBox1.DataBind();
+        GridView1.DataSource = obj;
+        GridView1.DataBind();
+    }
+}
